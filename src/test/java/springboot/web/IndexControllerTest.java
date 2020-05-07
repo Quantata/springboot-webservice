bb735b881dfbf3453667346b75e259ef1011f5f1
Quantata <ceoahn95@gmail.com>
2020-05-07 15:11:14 +0900

    Rename indexController to IndexController

--- src/test/java/springboot/web/IndexControllerTest.java
+++ src/test/java/springboot/web/IndexControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-public class indexControllerTest {
+public class IndexControllerTest {
 
 	@Autowired
 	private TestRestTemplate restTemplate;

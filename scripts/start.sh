e307628cbe6a84274d7d029749aac51517636bdb
Quantata <ceoahn95@gmail.com>
2020-05-12 13:29:08 +0900

    무중단 배포-war 파일 복사 IDLE_PORT 밑으로 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,13 +13,13 @@ cd $REPOSITORY
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.war $REPOSITORY/"
 IDLE_PORT=$(find_idle_port)
-cp $REPOSITORY/zip/*.war $REPOSITORY/${IDLE_PORT}
+cp $REPOSITORY/zip/*.war $REPOSITORY/${IDLE_PORT}/
 
-JAR_NAME=$(ls -tr $REPOSITORY/ | grep war | tail -n 1)
+JAR_NAME=$(ls -tr $REPOSITORY/${IDLE_PORT}/ | grep war | tail -n 1)
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> 새 애플리케이션($JAR_NAME) 배포"
 nohup java -jar \
 	-Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties -Dspring.profiles.active=$IDLE_PROFILE \
-	$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+	$JAR_NAME > $REPOSITORY/${IDLE_PORT}/nohup.out 2>&1 &
 
